27aad9b23a3821c705ccb4b00a749167402db5f3
shamu <javierobin.7@gmail.com>
2021-01-21 16:10:03 +0630

    Adding Pymongo additional courses

--- Pymongo_Course/main.py
+++ Pymongo_Course/main.py
@@ -26,10 +26,50 @@ users = db.users
 
 #counting
 
-print(users.find().count())
-print(users.find({"favourite_number":445}).count())
-print(users.find({"username":"shamu"}).count())
+# print(users.find().count())
+# print(users.find({"favourite_number":445}).count())
+# print(users.find({"username":"shamu"}).count())
+# print(users.find({"username":"ramu"}).count())
 
+
+#Multiple Find Conditions
+print(users.find({"username":"nick","favourite_number":445}).count())
+
+#Datetime and keywords
+
+import datetime
+
+current_date = datetime.datetime.now()
+print(current_date)
+old_date = datetime.datetime(2019,11,24)
+print(old_date)
+
+#insert users with date
+# uid = users.insert({"username":"bishnu", "date":current_date})
+# print(uid)
+#after insert users with the date, find query
+
+#gte => greater than or equal , lte => Less than or equal
+print(users.find({"date":{"$gte":old_date}}).count())
+print(users.find({"date":{"$lte":old_date}}).count())
+
+#this is for the date column existing lists
+print(users.find({"date":{"$exists":False}}).count())
+
+#$ne => Not Equal to
+
+#User table mr "shamu" so tat username net ma tu tat count
+print(users.find({"username":{"$ne":"shamu"}}).count())
+
+
+#Indexes => When having multiple query output for userlists, it will improve query speed
+
+index_created = db.users.create_index([('username',pymongo.ASCENDING)])
+print(index_created)
+
+#After that find related query. If there will have multiple users record
+print(users.find({"username":"nick"}))
+print(users.find({"username":"nick"}).count() == 2)
 
 
 
